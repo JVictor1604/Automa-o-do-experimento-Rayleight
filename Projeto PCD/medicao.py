0fee1b7f731e0f94edb8bb008d4fa600b7b7d448
JVictor1604 <josevictor8569@gmail.com>
2024-06-11 17:16:02 -0300

    TESTE

--- Projeto PCD/medicao.py
+++ Projeto PCD/medicao.py
@@ -1,26 +1,43 @@
-#!pip uninstall serial
-#}!pip install pySerial
+import os
 import serial
-# Lembrar de alterar o nome do arquivo sempre que iniciar um experimento novo
-NOME_DO_ARQUIVO = 'medida_espalhamento-31.txt'
-# Usar aqui o mesmo valor da velocidade de comunicação usada para iniciar a
-# comunicação serial no Arduino
-BAUD_RATE = 9600
-# Alterar o nome da porta serial para a porta onde o Arduino está conectado
-PORTA_SERIAL = 'COM15' # usar esta notação caso esteja no windows
-#PORTA_SERIAL = r'/dev/ttyACM0' # usar esta notação caso esteja no linux
+import serial.tools.list_ports
 
-# loop infinito
-#while True:
-for i in range(0,50):
-    # iniciando comunicação serial
-    with serial.Serial(PORTA_SERIAL, BAUD_RATE) as pserial:
-        # lendo a porta serial, decodificando e limpando a string
-        line = pserial.readline().decode('utf-8').rstrip().replace("Blue:","").replace("Red:","").replace("Green:","")
-        # exibindo a leitura da porta serial no terminal Python
-        print(line)
-        # escrevendo resultado em um arquivo
-        with open(NOME_DO_ARQUIVO, 'a') as arquivo:
-            arquivo.write(line + '\n')
+def identificar_porta_arduino():
+    """
+    Retorna a porta COM fornecida pelo usuário.
+    """
+    porta = input("Digite a porta COM à qual o Arduino está conectado (ex: COM3): ")
+    return porta
 
+def ler_dado_serial(porta_serial):
+    """
+    Lê um dado da porta serial e retorna a linha formatada.
+    """
+    BAUD_RATE = 9600  # Definindo a taxa de baud como 9600
+    with serial.Serial(porta_serial, BAUD_RATE, timeout=1) as pserial:
+        # Certifique-se de que há um timeout para que não fique esperando indefinidamente por dados
+        line = pserial.readline().decode('utf-8').rstrip()
+        return line
+with open(nome_do_arquivo, 'a') as arquivo:
+        arquivo.write(dado + '\n')
 
+def main():
+    # Criar a pasta 'dados' se ela não existir
+    if not os.path.exists('dados'):
+        os.makedirs('dados')
+
+    numero_arquivo = 1
+    numero_de_amostras = int(input("Digite o número de amostras que você deseja fazer: "))
+
+    # Solicitar ao usuário que insira a porta COM à qual o Arduino está conectado
+    PORTA_SERIAL = identificar_porta_arduino()
+
+    for i in range(numero_de_amostras):
+        NOME_DO_ARQUIVO = f'dados/medida_espalhamento-{numero_arquivo}.txt'
+        dado = ler_dado_serial(PORTA_SERIAL)
+        print(dado)  # Imprimir o dado lido da porta serial
+        salvar_dado_em_arquivo(NOME_DO_ARQUIVO, dado)
+        numero_arquivo += 1
+
+if __name__ == "__main__":
+    main()
