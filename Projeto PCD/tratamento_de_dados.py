dee70ef23b17f54e74ae60513bc6c1738aad294d
marcotlr <marcotlr2312@gmail.com>
2024-06-16 15:50:01 -0300

    teste

--- Projeto PCD/tratamento_de_dados.py
+++ Projeto PCD/tratamento_de_dados.py
@@ -1,21 +1,47 @@
-def projeto (caminho, N_de_gotas):
-    import os
+def tratar_dados(caminho, separador_de_coluna, intervalo_de_gotas, N_de_amostras):
+    import os 
     import glob as gb
     import pandas as pd
     import numpy as np
     
-    Padrão = caminho
-    gb.glob(padrão)
-    conjunto_de_dados = gb.glob(padrão)
+    padrao = os.path.join(caminho, "*.txt")
+    gb.glob(padrao)
+    conjunto_de_dados = gb.glob(padrao)
     
     
     contador = 0 
-    media_Vermelho = []
-    media_Verde = []
-    media_Azul = []
+    media_Vermelho = [0]
+    media_Verde = [0]
+    media_Azul = [0]
     desvio_Vermelho = []
     desvio_Verde = []
     desvio_Azul = []
-    
+    print(padrao)
+    print(conjunto_de_dados)
     for arquivo in conjunto_de_dados:
-        dataframe = pd.read_csv(arquivo, )
+        df = pd.read_csv(arquivo, delimiter = separador_de_coluna, names = ['Blue', 'Red', 'Green'])
+        if contador == 0:
+            media_Vermelho_referencia = df['Red'].mean()
+            desvio_Vermelho.append(df['Red'].sem())
+            media_Verde_referencia = df['Green'].mean()
+            desvio_Verde.append(df['Green'].sem())
+            media_Azul_referencia = df['Blue'].mean()
+            desvio_Azul.append(df['Blue'].sem())
+            contador +=1
+        else :
+            media_Vermelho_Unica = df['Red'].mean()
+            media_Vermelho.append(media_Vermelho_Unica - media_Vermelho_referencia)
+            desvio_Vermelho.append(df['Red'].sem())
+            media_Verde_Unica = df['Green'].mean()
+            media_Verde.append(media_Verde_Unica - media_Verde_referencia)
+            desvio_Verde.append(df['Green'].sem())
+            media_Azul_Unica = df['Blue'].mean()
+            media_Azul.append(media_Azul_Unica - media_Azul_referencia)
+            desvio_Azul.append(df['Blue'].sem())
+
+                
+    n_de_gotas = [ i * intervalo_de_gotas for i in range(N_de_amostras)]
+
+    return media_Azul, media_Verde, media_Vermelho, desvio_Azul, desvio_Verde, desvio_Vermelho
+    
+    
