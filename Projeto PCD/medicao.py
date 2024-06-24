9edbf1b21bfe5c0d98ac8243d01f8609605ef2a6
JVictor1604 <josevictor8569@gmail.com>
2024-06-24 14:28:34 -0300

    interface usuário

--- Projeto PCD/medicao.py
+++ Projeto PCD/medicao.py
@@ -10,6 +10,8 @@ if not os.path.exists('dados'):
 def medir_amostra(num_amostras, BAUD_RATE, PORTA_SERIAL):
     """Cria um arquivo txt para armazenar os dados e inicia a medição de cada amostra, que será registrada 50 vezes"""
     for numero_amostra in range(num_amostras):
+        if(numero_amostra == 0) :
+            str(input("Pressione Enter para iniciar as medições"))
         print(f"Iniciando medição da amostra {numero_amostra + 1}")
         time.sleep(0.5)  # Pequeno atraso para estabilizar a comunicação serial
         NOME_DO_ARQUIVO = f'dados/amostra-{numero_amostra + 1:02d}.txt'
@@ -26,7 +28,7 @@ def medir_amostra(num_amostras, BAUD_RATE, PORTA_SERIAL):
                 print(line)
                 # Adiciona a medição à lista de medições da amostra atual
                 dados_amostra.append(line)
-        if numero_amostra - 1  != num_amostras:
+        if numero_amostra  != num_amostras - 1:
             input("Pressione Enter para ir a próxima medição...")
         
         # Retorna o nome do arquivo e os dados da amostra
@@ -39,8 +41,6 @@ def criar_arquivo_com_medicoes(nome_do_arquivo, dados_das_amostras):
             arquivo.write(medicao + '\n')
 
 def medicao(num_de_amostras):
-    # Definindo o número de amostras
-    
     while True:
         try:
             Numero_da_Porta_Serial = int(input("Digite a porta serial à qual o Arduino está conectado (ex: 11): "))
@@ -57,9 +57,6 @@ def medicao(num_de_amostras):
         except serial.SerialException:
             print("Erro ao tentar conectar. Por favor, verifique se a porta está correta e tente novamente.")
 
-
-
-    # Executa a medição e cria os arquivos
     for nome_do_arquivo, dados_das_amostras in medir_amostra(num_de_amostras, BAUD_RATE=9600, PORTA_SERIAL=Porta_Serial):
         criar_arquivo_com_medicoes(nome_do_arquivo, dados_das_amostras)
 
